3efa3769ee171c2ac89a675aa91f4e7827ed8e16
Pedro Hernandez <pihebor@hotmail.com>
2020-06-02 21:48:16 -0500

    Calculo de Area Rectangulo y Cuadrado terminada con validaciones

--- app/src/main/java/com/example/appareavolumen/AreaCuadrado.java
+++ app/src/main/java/com/example/appareavolumen/AreaCuadrado.java
@@ -3,7 +3,6 @@ package com.example.appareavolumen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.service.autofill.FieldClassification;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +10,7 @@ import android.widget.TextView;
 public class AreaCuadrado extends AppCompatActivity {
     private double result_area, lado;
     private EditText txt_lado;
-    private TextView resultado;
+    private TextView resultado, lbl_area;
 
 
     @Override
@@ -20,22 +19,37 @@ public class AreaCuadrado extends AppCompatActivity {
         setContentView(R.layout.activity_area_cuadrado);
 
         txt_lado = findViewById(R.id.txtLado);
-        resultado = findViewById(R.id.lblMuestraResultado);
-        /*/lado = Double.parseDouble(txt_lado.getText().toString());
-        result_area = Math.pow(lado, 2);*/
-
-
+        resultado = findViewById(R.id.lblMuestraResultadoCua);
+        lbl_area = findViewById(R.id.lblResAreaCua);
+        lbl_area.setText("");
     }
 
-    public void calcular(View v){
-        lado = Double.parseDouble(txt_lado.getText().toString());
-        result_area = Math.pow(lado, 2);
-        resultado.setText("" + result_area);
+    public void calcular(View v) {
+        if (validar()) {
+            lado = Double.parseDouble(txt_lado.getText().toString());
+            lbl_area.setText("Area");
+            result_area = Math.pow(lado, 2);
+            resultado.setText("" + result_area);
+        }
     }
 
     public void borrar(View v){
         resultado.setText("");
         txt_lado.setText("");
+        lbl_area.setText("");
         txt_lado.requestFocus();
+    }
+
+    public boolean validar(){
+        boolean res;
+        String error_lado;
+        res = true;
+        error_lado = getResources().getString(R.string.error_lado);
+        if (txt_lado.getText().toString().isEmpty() || txt_lado.getText().toString().equals("0")){
+            txt_lado.setError(error_lado);
+            txt_lado.requestFocus();
+            res = false;
+        }
+        return res;
     }
 }
--- app/src/main/java/com/example/appareavolumen/AreaRectangulo.java
+++ app/src/main/java/com/example/appareavolumen/AreaRectangulo.java
@@ -3,12 +3,61 @@ package com.example.appareavolumen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AreaRectangulo extends AppCompatActivity {
-
+    private EditText txt_base, txt_altura;
+    private Double base, altura, resultado;
+    private TextView lbl_resultado, lbl_area;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_rectangulo);
+
+        txt_base = findViewById(R.id.txtBase);
+        txt_altura = findViewById(R.id.txtAltura);
+        lbl_resultado = findViewById(R.id.lblMuestraResultadoCua);
+        lbl_area = findViewById(R.id.lblResAreaRec);
+        lbl_area.setText("");
+    }
+
+    public void calcular(View v) {
+        if (validar()) {
+            base = Double.parseDouble(txt_base.getText().toString());
+            altura = Double.parseDouble(txt_altura.getText().toString());
+            resultado = Double.parseDouble(txt_altura.getText().toString());
+            resultado = base * altura;
+
+            lbl_area.setText("Area");
+            lbl_resultado.setText("" + resultado);
+        }
+    }
+        public void borrar (View v){
+            txt_base.setText("");
+            txt_altura.setText("");
+            lbl_resultado.setText("");
+            lbl_area.setText("");
+        }
+
+
+    public boolean validar(){
+        boolean res;
+        String error_base, error_altura;
+        res = true;
+        error_base = getResources().getString(R.string.error_base);
+        error_altura = getResources().getString(R.string.error_altura);
+        if (txt_base.getText().toString().isEmpty() || txt_base.getText().toString().equals("0")){
+            txt_base.setError(error_base);
+            txt_base.requestFocus();
+            res = false;
+        } else if (txt_altura.getText().toString().isEmpty() || txt_altura.getText().toString().equals("0")){
+            txt_altura.setError(error_altura);
+            txt_altura.requestFocus();
+            res = false;
+        }
+
+        return res;
     }
 }
