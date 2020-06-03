bcf93f8d1d171a717876c0d3992f1966f581a13f
Pedro Hernandez <pihebor@hotmail.com>
2020-06-03 18:58:40 -0500

    Se terminan todos los calculos de Area de las figuras, con validaciones. Iniciamos la parte de calculo de volumenes

--- app/src/main/java/com/example/appareavolumen/AreaCirculo.java
+++ app/src/main/java/com/example/appareavolumen/AreaCirculo.java
@@ -3,12 +3,53 @@ package com.example.appareavolumen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AreaCirculo extends AppCompatActivity {
+    private EditText txt_radio;
+    private Double radio, resultado_cir;
+    private TextView lbl_resultado_cir, lbl_area_cir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_circulo);
+
+        txt_radio = findViewById(R.id.txtRadio);
+        lbl_resultado_cir = findViewById(R.id.lblMuestraResultadoCir);
+        lbl_area_cir = findViewById(R.id.lblResAreaCir);
+        lbl_area_cir.setText("");
+    }
+
+    public void calcular(View v) {
+        if (validar()) {
+            radio = Double.parseDouble(txt_radio.getText().toString());
+            //resultado_cir = Double.parseDouble(txt_altura.getText().toString());
+            resultado_cir = Math.PI*(Math.pow(radio,2));
+
+            lbl_area_cir.setText("Area");
+            lbl_resultado_cir.setText("" + resultado_cir);
+        }
+    }
+
+    public void borrar (View v){
+        txt_radio.setText("");
+        lbl_resultado_cir.setText("");
+        lbl_area_cir.setText("");
+    }
+
+    public boolean validar(){
+        boolean res;
+        String error_radio;
+        res = true;
+        error_radio = getResources().getString(R.string.error_radio);
+        if (txt_radio.getText().toString().isEmpty() || txt_radio.getText().toString().equals("0")){
+            txt_radio.setError(error_radio);
+            txt_radio.requestFocus();
+            res = false;
+        }
+        return res;
     }
 }
